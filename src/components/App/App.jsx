012d0e2157a6ccb968b01bc5768860e7e0d53288
YuliiaHosteva <140606153+YuliiaHosteva@users.noreply.github.com>
2024-02-23 18:57:37 +0100

    Summary work

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,40 +1,56 @@
+import { useState, useEffect } from 'react';
 import './App.css';
-import { Navigation } from '../Navigation/Navigation';
-import { Profile } from '../Profile/Profile';
-import { FriendList } from '../FriendList/FriendList';
-import { TransactionHistory } from '../TransactionHistory/TransactionHistory';
-import userData from '../../backend/userData.json';
-import friends from '../../backend/friends.json';
-import transactions from '../../backend/transactions.json';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Options from '../Options/Options';
+import Feedback from '../Feedback/Feedback';
+import Notification from '../Notification/Notification';
+import Description from '../Description/Description';
 
-function App() {
+const App = () => {
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  });
+
+  useEffect(() => {
+    const savedFeedback = localStorage.getItem('feedback');
+    if (savedFeedback) {
+      setFeedback(JSON.parse(savedFeedback));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('feedback', JSON.stringify(feedback));
+  }, [feedback]);
+
+  const updateFeedback = (feedbackType) => {
+    setFeedback(prevFeedback => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1
+    }));
+  };
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+
+  const resetFeedback = () => {
+    setFeedback({
+      good: 0,
+      neutral: 0,
+      bad: 0
+    });
+  };
+
   return (
-    <>
-      <Router>
-        <Navigation />
-        <Routes>
-          <Route
-            path="/profile"
-            element={
-              <Profile
-                username={userData.username}
-                avatar={userData.avatar}
-                location={userData.location}
-                tag={userData.tag}
-                stats={userData.stats}
-              />
-            }
-          />
-          <Route path="/friend-list" element={<FriendList friends={friends} />} />
-          <Route
-            path="/transaction-history"
-            element={<TransactionHistory items={transactions} />}
-          />
-        </Routes>
-      </Router>
-    </>
+    <div>
+    <Description />      
+    <Options updateFeedback={updateFeedback} resetFeedback={resetFeedback} totalFeedback={totalFeedback} />
+      {totalFeedback > 0 ? (
+        <Feedback feedback={feedback} totalFeedback={totalFeedback} />
+      ) : (
+        <Notification message="No feedback given yet." />
+      )}
+    </div>
   );
-}
+};
 
 export default App;
